890b8c728595894b6be1dd0cc27ff37845bd1cc1
Fitri Zuyina Nur Azizah <nuranoer7@gmail.com>
2023-07-02 08:24:37 +0700

    Final

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,16 @@ import { SupplierModule } from './supplier/supplier.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'db_toko',
+      entities: ["src/**/*.entity.ts"],
+      synchronize: true,
+    }),
     PurchaseModule,
     KasirModule,
     ProductModule,
